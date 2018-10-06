6fd5fcae586f473d7fd52042e53fbecaead87726
Dana Palmer <roiai4@hotmail.com>
2018-10-06 17:09:56 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,5 @@
 def speak_to_grandma(phrase)
   if phrase == phrase.upcase
      "NO, NOT SINCE 1938!"
-  elsif phrase == phrase.downcase
-     "HUH?! SPEAK UP, SONNY!"
-  else phrase == "I LOVE YOU GRANDMA!"
-     "I LOVE YOU TOO PUMPKIN!"
-  end
+end
 end
