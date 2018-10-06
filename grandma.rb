4462d70276584802811dc69b6dbe6a799f6fad04
Dana Palmer <roiai4@hotmail.com>
2018-10-06 17:00:51 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,9 +1,9 @@
 def speak_to_grandma(phrase)
   if phrase.downcase
-    return "HUH?! SPEAK UP, SONNY!"
+     "HUH?! SPEAK UP, SONNY!"
   elsif phrase.upcase
-    return "NO, NOT SINCE 1938!"
+     "NO, NOT SINCE 1938!"
   else "I love you grandma!".upcase
-    return "I LOVE YOU TOO PUMPKIN!"
+     "I LOVE YOU TOO PUMPKIN!"
   end
 end
