e1d475a04ac764e2a3960d98e7f561c9af339d51
Dana Palmer <roiai4@hotmail.com>
2018-10-06 16:57:39 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,5 +1,9 @@
 def speak_to_grandma
   if "I love you grandma"
     puts "HUH?! SPEAK UP, SONNY!"
-
+  elsif "hi".upcase
+    puts "NO, NOT SINCE 1938!"
+  else "I love you grandma!".upcase
+    puts "I LOVE YOU TOO PUMPKIN!"
   end
+end
